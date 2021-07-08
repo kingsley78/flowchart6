a8f390ede139c6031a825523aef9a24fe056d725
kingsley78 <85633140+kingsley78@users.noreply.github.com>
2021-07-08 08:07:14 +0100

    Add files via upload

---
+++ flow6.py
@@ -0,0 +1,10 @@
+#flowchart6
+name = input("enter name")
+
+if name == "alice":
+    print("hi alice")
+else:
+    age = int(input("please enter age"))
+    if age < 12:
+        print("you are not alice kiddo")
+    else:print("yopu are neither alice nor a little kid")
